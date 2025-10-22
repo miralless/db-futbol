5c032e08531681e9f22bcc53dc6f705320386c7c
miralless <mirallesgaizka@gmail.com>
2025-10-22 13:38:40 +0200

    leak update

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDpjK4HsS4X4XmTzdjnRTTjlPJBuXzXAw4",
+    authDomain: "db-futbol.firebaseapp.com",
+    projectId: "db-futbol",
+    storageBucket: "db-futbol.appspot.com",
+    messagingSenderId: "533304962889",
+    appId: "1:533304962889:web:86243092a1ddcf89323498"
+};
